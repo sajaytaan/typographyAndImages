650132ea84356426812370b61e27ea072e6e7362
unknown <aytaantradinginc@gmail.com>
2024-01-27 22:05:10 +0800

    first commit

---
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+const PhotoGridItem = ({
+  id,
+  src,
+  alt,
+  tags,
+  srcSetAvif,
+  srcSetJpg,
+}) => {
+  return (
+    <article>
+      <Anchor href={`/photos/${id}`}>
+        <picture>
+          <source type='image/avif' srcSet={srcSetAvif} />
+          <source type='image/jpg' srcSet={srcSetJpg} />
+          <Image src={src} alt={alt} />
+        </picture>
+      </Anchor>
+      <Tags>
+        {tags.map((tag) => (
+          <Tag key={tag}>{tag}</Tag>
+        ))}
+      </Tags>
+    </article>
+  );
+};
+
+const Anchor = styled.a`
+  text-decoration: none;
+  color: inherit;
+  outline-offset: 4px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 100%;
+  height: 300px;
+  border-radius: 2px;
+  margin-bottom: 8px;
+  object-fit: cover;
+`;
+
+const Tags = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+const Tag = styled.li`
+  padding: 4px 8px;
+  background: var(--color-gray-300);
+  font-size: 0.875rem;
+  font-weight: 475;
+  color: var(--color-gray-800);
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+export default PhotoGridItem;
---
+++ src/data.js
@@ -0,0 +1,102 @@
+const data = [
+  {
+    id: "a",
+    src: "/images/vincent-van-zalinge-bird.jpg",
+    srcSetAvif:
+      "/images/vincent-van-zalinge-bird.avif 1x,/images/vincent-van-zalinge-bird@2x.avif 2x,/images/vincent-van-zalinge-bird@3x.avif 3x",
+    srcSetJpg:
+      "/images/vincent-van-zalinge-bird.jpg 1x,/images/vincent-van-zalinge-bird@2x.jpg 2x,/images/vincent-van-zalinge-bird@3x.jpg 3x",
+    alt: "A tiny shrewd turquoise-and-amber bird",
+    tags: [
+      "bird",
+      "blurred background",
+      "wildlife",
+      "animal in its natural habitat",
+    ],
+  },
+  {
+    id: "b",
+    src: "/images/alexandru-rotariu-dog.jpg",
+    srcSetAvif:
+      "/images/alexandru-rotariu-dog.avif 1x,/images/alexandru-rotariu-dog@2x.avif 2x,/images/alexandru-rotariu-dog@3x.avif 3x",
+    srcSetJpg:
+      "/images/alexandru-rotariu-dog.jpg 1x,/images/alexandru-rotariu-dog@2x.jpg 2x,/images/alexandru-rotariu-dog@3x.jpg 3x",
+    alt: "A serious-looking grey dog with cool glacier eyes",
+    tags: ["dog", "HD"],
+  },
+  {
+    id: "c",
+    src: "/images/scott-walsh-fox.jpg",
+    srcSetAvif:
+      "/images/scott-walsh-fox.avif 1x,/images/scott-walsh-fox@2x.avif 2x,/images/scott-walsh-fox@3x.avif 3x",
+    srcSetJpg:
+      "/images/scott-walsh-fox.jpg 1x,/images/scott-walsh-fox@2x.jpg 2x,/images/scott-walsh-fox@3x.jpg 3x",
+    alt: "A happy-looking cute wild fox in nature, near some pink flowers",
+    tags: ["fox", "flowers"],
+  },
+  {
+    id: "d",
+    src: "/images/andy-holmes-giraffe.jpg",
+    srcSetAvif:
+      "/images/andy-holmes-giraffe.avif 1x,/images/andy-holmes-giraffe@2x.avif 2x,/images/andy-holmes-giraffe@3x.avif 3x",
+    srcSetJpg:
+      "/images/andy-holmes-giraffe.jpg 1x,/images/andy-holmes-giraffe@2x.jpg 2x,/images/andy-holmes-giraffe@3x.jpg 3x",
+    alt: "A giraffe sticking its black tongue out",
+    tags: ["giraffe", "dramatic"],
+  },
+  {
+    id: "e",
+    src: "/images/karsten-winegeart-dog.jpg",
+    srcSetAvif:
+      "/images/karsten-winegeart-dog.avif 1x,/images/karsten-winegeart-dog@2x.avif 2x,/images/karsten-winegeart-dog@3x.avif 3x",
+    srcSetJpg:
+      "/images/karsten-winegeart-dog.jpg 1x,/images/karsten-winegeart-dog@2x.jpg 2x,/images/karsten-winegeart-dog@3x.jpg 3x",
+    alt: 'A small dog wearing a golden "Champions" hoodie',
+    tags: ["dog", "cute", "animal wearing human clothes"],
+  },
+  {
+    id: "f",
+    src: "/images/marko-blazevic-cat.jpg",
+    srcSetAvif:
+      "/images/marko-blazevic-cat.avif 1x,/images/marko-blazevic-cat@2x.avif 2x,/images/marko-blazevic-cat@3x.avif 3x",
+    srcSetJpg:
+      "/images/marko-blazevic-cat.jpg 1x,/images/marko-blazevic-cat@2x.jpg 2x,/images/marko-blazevic-cat@3x.jpg 3x",
+    alt: "A small kitten standing on its back legs, reaching up towards the camera",
+    tags: ["kitten", "cat", "#cute"],
+  },
+  {
+    id: "g",
+    src: "/images/mark-stoop-lizard.jpg",
+    srcSetAvif:
+      "/images/mark-stoop-lizard.avif 1x,/images/mark-stoop-lizard@2x.avif 2x,/images/mark-stoop-lizard@3x.avif 3x",
+    srcSetJpg:
+      "/images/mark-stoop-lizard.jpg 1x,/images/mark-stoop-lizard@2x.jpg 2x,/images/mark-stoop-lizard@3x.jpg 3x",
+    alt: "A relaxed green lizard, sitting on a wooden beam",
+    tags: [
+      "lizard",
+      "world-famous insurance salesman from the commercials",
+    ],
+  },
+  {
+    id: "h",
+    src: "/images/geran-de-klerk-squirrel.jpg",
+    srcSetAvif:
+      "/images/geran-de-klerk-squirrel.avif 1x,/images/geran-de-klerk-squirrel@2x.avif 2x,/images/geran-de-klerk-squirrel@3x.avif 3x",
+    srcSetJpg:
+      "/images/geran-de-klerk-squirrel.jpg 1x,/images/geran-de-klerk-squirrel@2x.jpg 2x,/images/geran-de-klerk-squirrel@3x.jpg 3x",
+    alt: "A fuzzy squirrel, highlighted in a dark backdrop",
+    tags: ["squirrel", "animal", "fuzzy"],
+  },
+  {
+    id: "i",
+    src: "/images/wexor-tmg-turtle.jpg",
+    srcSetAvif:
+      "/images/wexor-tmg-turtle.avif 1x,/images/wexor-tmg-turtle@2x.avif 2x,/images/wexor-tmg-turtle@3x.avif 3x",
+    srcSetJpg:
+      "/images/wexor-tmg-turtle.jpg 1x,/images/wexor-tmg-turtle@2x.jpg 2x,/images/wexor-tmg-turtle@3x.jpg 3x",
+    alt: "A large tropical turtle swimming in water",
+    tags: ["turtle", "ocean", "flippers"],
+  },
+];
+
+export default data;
